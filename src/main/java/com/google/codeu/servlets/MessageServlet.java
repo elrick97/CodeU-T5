3bbdfc7cfef81bbe8db316c8733d762d1a3bf3ef
Ricardo Ramierz <ricardo.rmz.97@hotmail.com>
2019-03-05 19:08:24 -0600

    Posting message error fixed, stats html file fixed

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -79,9 +79,6 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
     String recipient = request.getParameter("recipient");
-
-    String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
-
     Message message = new Message(user, text, recipient);
     datastore.storeMessage(message);
 
