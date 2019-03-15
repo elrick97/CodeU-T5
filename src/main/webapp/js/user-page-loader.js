2410fb9802a1774d1853b814afbb62d1a5cea9d8
Ricardo Ramierz <ricardo.rmz.97@hotmail.com>
2019-03-15 16:26:03 -0700

    Update user-page-loader.js

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -98,8 +98,6 @@ function buildMessageDiv(message) {
   bodyDiv.classList.add('message-body');
   bodyDiv.innerHTML = isBlockCode(message.text);
 
-  console.log(message.tag);
-
   const messageDiv = document.createElement('div');
   messageDiv.classList.add('message-div');
   messageDiv.appendChild(headerDiv);
